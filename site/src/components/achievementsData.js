6a0029a6a9f2ae730fdfc35ad0ed681efea485d0
Casey Lewiston <lewiston@sonoma.edu>
2020-05-12 14:28:46 -0700

    Badge updates

--- site/src/components/achievementsData.js
+++ site/src/components/achievementsData.js
@@ -29,6 +29,12 @@ export const badgeData = {
             name: "Airporter",
             desc: "Reached San Francisco International Airport."
         },
+        {
+            mile: 30,
+            image: "jailer.png",
+            name: "The Jailer",
+            desc: "Stopped by San Quentin State Prison after crossing the Richmond Bridge."
+        }
 
     ],
 
@@ -58,6 +64,25 @@ export const badgeData = {
             name: "Googler",
             desc: "Visited the Googleplex."
         },
+        {
+            mile: 58,
+            image: "railroad.png",
+            name: "Ghost Buster",
+            desc: "Went to the railroad ghost town of Drawbridge."
+        },
+        {
+            mile: 97,
+            image: "ikea.png",
+            name: "L\u00e4ndenv\u00fcagr",
+            desc: "Stopped by Ikea in Emeryville. Meatballs and furniture, everything you need!"
+        },
+        {
+            mile: 108,
+            image: "rosietheriveter.png",
+            name: "Riveter",
+            desc: "Visited the Rosie the Riveter Museum."
+        },
+
 
 
     ]
