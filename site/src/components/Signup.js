87acd2e63355645470edfa6cf21c50167ae239e2
Casey Lewiston <lewiston@sonoma.edu>
2020-05-12 13:06:45 -0700

    Description

--- site/src/components/Signup.js
+++ site/src/components/Signup.js
@@ -49,8 +49,16 @@ class Signup extends React.Component {
       <div className="App">
         <div className="introText">
           <p>Skate the Bay is a virtual skate marathon hosted by <a href="https://rrderby.org" target="_new">Resurrection Roller Derby</a>, open to skaters everywhere.
-          The route comes in three types: Mini, which is equivalent to traveling across the Golden Gate Bridge and back; Bridging, which is the distance it takes to
-          loop around the Golden Gate, Richmond and Bay bridges; and the Full Bay, which goes around the San Francisco and San Pablo bays. There's no entry fee for participation, but we
+          The route comes in three types.
+          <br/>
+          <br />
+          Mini: Equivalent to traveling across the Golden Gate Bridge and back.
+          <br /><br />
+          Bridging: An architectural tour of the Bay Area's most prominent bridges, equivalent to crossing the historic Golden Gate Bridge, the modern Bay Bridge, and the Richmond Bridge, which is also a bridge.
+          <br /><br/>
+          Full Bay: Goes around the San Francisco and San Pablo bays. 
+          <br/><br/>
+          There's no entry fee for participation, but we
           encourage any skaters who can to donate to their local food bank. You can visit <a href="https://www.feedingamerica.org/find-your-local-foodbank" target="_new">Feeding America </a>
           to find yours.
           <br />
