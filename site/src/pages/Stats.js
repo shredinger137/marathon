f057fc3496ee515393c6a8cedc0b4487e9da1aed
Casey Lewiston <lewiston@sonoma.edu>
2020-06-26 22:29:30 -0700

    Style

--- site/src/pages/Stats.js
+++ site/src/pages/Stats.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../App.css';
 import '../css/common.css'
-import {config} from '../config';
+import { config } from '../config';
 import axios from 'axios';
 
 class Stats extends React.Component {
@@ -16,46 +16,46 @@ class Stats extends React.Component {
     completedFull: []
   };
 
-  componentDidMount(){
-      this.getStats();
+  componentDidMount() {
+    this.getStats();
   }
 
-  getStats(){
+  getStats() {
     axios.get(`${config.api}/getstats`).then(res => {
-        var statData;
-        var completedData;
-        var graphData = [];
-        if(res && res.data && res.data[0]){
-            statData = res.data[0];
-        }
-        //This is super ugly and should probably be rethought.
-        if(res && res.data && res.data[1]){
-          completedData = res.data[1];
-          console.log(completedData["completedFullMarathon"]);
-        }
+      var statData;
+      var completedData;
+      var graphData = [];
+      if (res && res.data && res.data[0]) {
+        statData = res.data[0];
+      }
+      //This is super ugly and should probably be rethought.
+      if (res && res.data && res.data[1]) {
+        completedData = res.data[1];
+        console.log(completedData["completedFullMarathon"]);
+      }
 
-        if(statData["combinedMiles"]){
-            this.setState({combinedMiles: statData["combinedMiles"]});
+      if (statData["combinedMiles"]) {
+        this.setState({ combinedMiles: statData["combinedMiles"] });
+      }
+      if (statData["totalUsers"]) {
+        this.setState({ totalUsers: statData["totalUsers"] });
+      }
+      if (statData["leaderBoardByDistance"]) {
+        this.setState({ leaderboard: statData["leaderBoardByDistance"] });
+        console.log(statData["leaderBoardByDistance"])
+      }
+      if (completedData && completedData["completedFullMarathon"]) {
+        this.setState({ completedFull: completedData["completedFullMarathon"] });
+        console.log(completedData["completedFullMarathon"]);
+      }
+      if (statData["distanceByDate"]) {
+        this.setState({ distanceByDate: statData["distanceByDate"] });
+        var allDates = Object.keys(statData["distanceByDate"]);
+        for (var date of allDates) {
+          graphData.push(statData.distanceByDate[date]);
         }
-        if(statData["totalUsers"]){
-            this.setState({totalUsers: statData["totalUsers"]});
-        }
-        if(statData["leaderBoardByDistance"]){
-          this.setState({leaderboard: statData["leaderBoardByDistance"]});
-          console.log(statData["leaderBoardByDistance"])
-        }
-        if(completedData && completedData["completedFullMarathon"]){
-          this.setState({completedFull: completedData["completedFullMarathon"]});
-          console.log(completedData["completedFullMarathon"]);
-        }
-        if(statData["distanceByDate"]){
-            this.setState({distanceByDate: statData["distanceByDate"]});
-            var allDates = Object.keys(statData["distanceByDate"]);
-            for(var date of allDates){
-                graphData.push(statData.distanceByDate[date]);
-            }
-            this.setState({distanceGraphData: graphData, distanceGraphLabels: allDates});
-        }
+        this.setState({ distanceGraphData: graphData, distanceGraphLabels: allDates });
+      }
 
     })
   }
@@ -64,49 +64,56 @@ class Stats extends React.Component {
   render() {
     return (
       <div className="App">
-          <p>Participants: <strong>{this.state.totalUsers}</strong>
+        <p>Participants: <strong>{this.state.totalUsers}</strong>
           <br />
           Combined Miles: <strong>{this.state.combinedMiles}</strong></p>
-          <br />
-          <h3>Leaderboards</h3>
-          <span><b>Overall Distance</b></span>
-          <table className="leaderboard">
-            <tbody>
-            {this.state.leaderboard.map(
-                                user => (
-                                  <tr key={user.name}>
-                                    <td>
-                                      <span className="small">{user.name}</span>
-                                    </td>
-                                    <td><span className="small">{user.totalDistance}{" "}Miles</span></td>
-                                  </tr>
-                                )
+        <br />
+        <h3>Leaderboards</h3>
 
-                            )}
+        <div className="statsWrapper">
+          <div>
+            <span><b>Overall Distance</b></span>
+            <br /><br />
+            <table className="leaderboard">
+              <tbody>
+                {this.state.leaderboard.map(
+                  user => (
+                    <tr key={user.name}>
+                      <td>
+                        <span className="small">{user.name}</span>
+                      </td>
+                      <td><span className="small">{user.totalDistance}{" "}Miles</span></td>
+                    </tr>
+                  )
 
-            </tbody>  
-          </table> 
-          <br /><br />
-          <span><b>Hall of Heroes</b></span>
-          <br />
-          <span><i>These brave skaters have completed or surpassed the challenge of the Full Bay Marathon.</i></span>
-          <br /><br />
-          <table className="leaderboard">
-            <tbody>
-            {this.state.completedFull.map(
-                                user => (
-                                  <tr key={user.name + 'completed'}>
-                                    <td>
-                                      <span className="small">{user.name}</span>
-                                    </td>
-                                    <td><span className="small">{user.totalDistance}{" "}Miles</span></td>
-                                  </tr>
-                                )
+                )}
 
-                            )}
+              </tbody>
+            </table>
+          </div>
+          <div>
+            <span><b>Hall of Heroes</b></span>
+            <br /><br />
+            <span><i>These brave skaters have completed or surpassed the challenge of the Full Bay Marathon.</i></span>
+            <br /><br />
+            <table className="leaderboard">
+              <tbody>
+                {this.state.completedFull.map(
+                  user => (
+                    <tr key={user.name + 'completed'}>
+                      <td>
+                        <span className="small">{user.name}</span>
+                      </td>
+                      <td><span className="small">{user.totalDistance}{" "}Miles</span></td>
+                    </tr>
+                  )
 
-            </tbody>  
-          </table> 
+                )}
+
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     );
   }
